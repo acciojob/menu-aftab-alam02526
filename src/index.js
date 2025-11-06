5846a912567e1bccd9730cf7f050a448f49b7b5e
aftab-alam02526 <aftab0447@gmail.com>
2025-11-06 23:46:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+// This path is CRITICAL.
+// It must point to your App.js file inside the 'components' folder.
+import App from './components/App';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
